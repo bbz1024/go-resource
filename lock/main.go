ce5ab0d0e2c098edc86c8cd3d286811c312a3d54
bbz <2632141215@qq.com>
2024-04-10 13:36:37 +0800

    lock

--- lock/main.go
+++ lock/main.go
@@ -1,27 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"strconv"
-	"strings"
-)
-
 func main() {
-
-	for i := 0; i < 200; i++ {
-		go func() {
-			fmt.Println(GoID())
-		}()
-	}
-}
-func GoID() int {
-	var buf [64]byte
-	n := runtime.Stack(buf[:], false)
-	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
-	id, err := strconv.Atoi(idField)
-	if err != nil {
-		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
-	}
-	return id
 }
